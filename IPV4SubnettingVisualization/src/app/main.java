5360cf19287728c48f97cb22ab22ca86e5e67ebc
samso <samso@10.220.90.203>
2025-11-05 11:05:30 -0500

    Test

---
+++ IPV4SubnettingVisualization/src/app/main.java
@@ -0,0 +1,44 @@
+package app;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+// may be used Later
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class main
+{
+	
+	public static void main(String[] args)
+	{
+		// Checking if we can push
+	}
+	
+}
